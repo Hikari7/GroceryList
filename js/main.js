421f8212dbafe1ef882f3a3c07a677f2041db61d
hiKobe <102020671+Hikari7@users.noreply.github.com>
2022-06-29 12:42:27 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const inputAdd = document.getElementById("add_item");
 const inputNum = document.getElementById("add_num");
-// const inputItem = document.getElementsByClassName("input_item");
 const add = document.querySelector(".add");
 const listItem = document.querySelector(".list_item"); // li
 const listLine = document.querySelector(".list"); // ul
@@ -17,18 +16,27 @@ let isInputed = false;
 //   num = e.target.value;
 // });
 
+function inputCheck() {
+  if (item.form_item.value == "") {
+    alert("Please enter the fields!");
+    return false;
+  } else {
+    return true;
+  }
+}
+
 add.addEventListener("click", (e) => {
-    if(isInputed) {
-        console.log("入力してください");
-    } 
   e.preventDefault(); //ページ遷移防ぐ
+  // return inputCheck();
+  //出力はできたけど、、
+  // if (isInputed) {  //入力されていなかったら
+  //   console.log("入力してください");
+  // } else {
   const itemName = document.createElement("span"); //span of item
   const itemNum = document.createElement("span"); //span of num
   itemName.textContent = inputAdd.value;
   itemNum.textContent = inputNum.value;
+  check.after(itemNum);
   check.after(itemName);
-  plus.before(itemNum);
-  list.push(list_item);
-  this.reset();
-});
 
+});
