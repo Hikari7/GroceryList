054c4238d6683b1285a1ead1fd6ead5ecef33d08
Hikari7 <h.kobe712@gmail.com>
2022-07-03 13:38:37 -0700

    status

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@ const inputNum = document.getElementById("add_num");
 const lists = document.querySelector(".list"); // ul
 const check = document.getElementById("check");
 const items = [];
+const del = document.querySelector("material-symbols-outlined delete");
+
+// document.querySelector(".material-symbols-outlined delete").innerHTML = '<span class="material-symbols-outlined delete">delete</span> '
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault(); //ページ遷移防ぐ
 
   const listItem = document.createElement("li");
   listItem.classList.add("inputed_info");
-  console.log(inputName);
+
   listItem.innerHTML = `<li class="list_item">
   <input type="checkbox" class="check" />
   <span class="inputed_name">
@@ -19,10 +22,30 @@ addBtn.addEventListener("click", (e) => {
   <span class="inputed_num">
   ${inputNum.value}
   </span>
-  <span class="material-symbols-outlined">delete</span>
+  <span class="material-symbols-outlined delete">delete</span>
+  <span class="material-symbols-outlined edit">
+edit_note
+</span>
   </li>`;
 
   inputName.value = "";
   inputNum.value = "";
+
   lists.appendChild(listItem);
+
+  const edits = document.querySelectorAll(".edit").forEach((edit) => {
+    edit.addEventListener("click", editBtn);
+  });
+
+  const deletes = document.querySelectorAll(".delete").forEach((del) => {
+    del.addEventListener("click", delBtn);
+  });
 });
+
+function editBtn() {
+  // console.log("編集してやるぞ");
+}
+
+function delBtn() {
+  // console.log("削除してやるで");
+}
