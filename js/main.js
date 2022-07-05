d4c002e579a85eaf16ad44dad70de725a010a863
Hikari7 <h.kobe712@gmail.com>
2022-07-05 11:52:12 -0700

    implemented del btn

--- js/main.js
+++ js/main.js
@@ -2,96 +2,44 @@ const addBtn = document.querySelector(".add"); //addするボタン
 const inputName = document.getElementById("add_item");
 const inputNum = document.getElementById("add_num");
 const lists = document.querySelector(".list"); // ul
-// const items = [];
-
-// addBtn.addEventListener("click", (e) => {
-// const listItem = document.createElement("li");
-// const addList = function (e) {
-//   e.preventDefault(); //ページ遷移防ぐ
-
-//   listItem.innerHTML = `<li class="list_item">
-//     <input type="checkbox" class="check" />
-//     <span class="inputed_name">
-//     ${inputName.value}
-//     </span>
-//     <span class="inputed_num">
-//     ${inputNum.value}
-//     </span>
-//     <span class="material-symbols-outlined delete">delete</span>
-//     <span class="material-symbols-outlined edit">
-//   edit_note
-//   </span>
-//     </li>`;
-
-//   inputName.value = "";
-//   inputNum.value = "";
-
-//   const edits = document.querySelectorAll(".edit").forEach((edit) => {
-//     edit.addEventListener("click", editBtn);
-//   });
-
-//   const deletes = document.querySelectorAll(".delete").forEach((del) => {
-//     del.addEventListener("click", delBtn);
-//   });
-// };
-// lists.appendChild(listItem);
-
-// addBtn.addEventListener("click", addList);
-// items.push(listItem);
-// console.log(items);
 
 //functionは外だったら反映するので、一旦引数に渡して外で内容を書いていく
-// const items = [];
 
 addBtn.addEventListener("click", addList);
 
 function addList(e) {
-  e.preventDefault(); //ページ遷移防ぐ
-  const listItem = document.createElement("li");
-  // console.log(listItem);
+  e.preventDefault();
+  const listItem = document.createElement("li"); //create "li" element
   const items = [
-    (listItem.innerHTML = `<li class="list_item">
-  <input type="checkbox" class="check" />
-  <span class="inputed_name">
-  ${inputName.value}
-  </span>
-  <span class="inputed_num">
-  ${inputNum.value}
-  </span>
-  <span class="material-symbols-outlined delete">delete</span>
-  <span class="material-symbols-outlined edit">
-  edit_note
-  </span>
-  </li>`),
+    (listItem.innerHTML = `
+    <input type="checkbox" class="check" />
+    <span class="inputed_name">
+    ${inputName.value}
+    </span>
+    <span class="inputed_num">
+    ${inputNum.value}
+    </span>
+    <span class="material-symbols-outlined delete">delete</span>
+    <span class="material-symbols-outlined edit">
+    edit_note
+    </span>
+    `),
   ];
 
+  listItem.classList.add("list_item");
   inputName.value = "";
   inputNum.value = "";
 
   lists.append(listItem);
 
   const deletes = document.querySelectorAll(".delete").forEach((del) => {
-    for (let item = 0; item < items.length; item++) {
-      del.addEventListener("click", () => {
-        // lists.removeChild(listItem);
-        // lists.remove(listItem);
-        items.splice(items.indexOf(item), 1);
-        // console.log(item);
-      });
-      // console.log(items);
-    }
+    del.addEventListener("click", delBtn);
   });
 }
 
-// console.log(item);
-
-// const edits = document.querySelectorAll(".edit").forEach((edit) => {
-//   edit.addEventListener("click", editBtn);
-// });
-
-//
-
-//
+function delBtn() {
+  lists.removeChild(this.parentNode); //parentNode = "ul"
+}
 
 // function editBtn() {
 //   // console.log("編集してやるで");
