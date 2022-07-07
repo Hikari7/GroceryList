1c7b90d27a5f8fcd404c8803f2a3488b7ac85303
Hikari7 <h.kobe712@gmail.com>
2022-07-07 12:44:06 -0700

    added edit mode

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const addBtn = document.querySelector(".add"); //addするボタン
 const inputName = document.getElementById("add_item");
 const inputNum = document.getElementById("add_num");
 const lists = document.querySelector(".list"); // ul
+// const editList = document.querySelector('.edit_list')
 
 //functionは外だったら反映するので、一旦引数に渡して外で内容を書いていく
 
@@ -14,19 +15,18 @@ function addList(e) {
     (listItem.innerHTML = `
     <input type="checkbox" class="check" />
     <span class="inputed_name">
-    ${inputName.value}
+    ${inputName.value} ${inputNum.value}
     </span>
-    <span class="inputed_num">
-    ${inputNum.value}
-    </span>
+    <form class="edit">
+          <input type="text" />
+          <input type="number" />
+        </form>
     <span class="material-symbols-outlined delete">delete</span>
-    <span class="material-symbols-outlined edit">
+    <span class="material-symbols-outlined edit_btn">
     edit_note
     </span>
     `),
   ];
-
-  // console.log(willbechecked);
 
   listItem.classList.add("list_item");
   inputName.value = "";
@@ -34,65 +34,54 @@ function addList(e) {
 
   lists.append(listItem);
 
-  // const checked = document.querySelectorAll(".check").forEach((ch) => {
-  //   ch.addEventListener("click", () => {
-  //     // console.log("checked!", listItem);
-  //     // listItem.classList.add("done");
-  //     document.querySelectorAll(".inputed_name").forEach((el) => {
-  //       console.log(el);
-  //       el.classList.add("done");
-  //     });
-  //   });
-  // });
+  // inputName.setAttribute('input', 'readonly');
 
   const deletes = document.querySelectorAll(".delete").forEach((del) => {
     del.addEventListener("click", delBtn);
   });
 
-  const edits = document.querySelectorAll(".edit").forEach((edit) => {
-    edit.addEventListener("click", editBtn);
+  const editBtn = document.querySelectorAll(".edit_btn").forEach((edi) => {
+    edi.addEventListener("click", editMode);
   });
+
+  function editMode(e) {
+    //この時点でeditボタン押したことになってる
+    console.log("editしてやるで");
+    const edits = document.querySelectorAll(".edit").forEach((edit) => {
+      edit.classList.remove("edit_list"); //formからのDOMを取るようにする
+      //valueを更新する
+      
+    });
+  }
 }
 
-// function checked() {
-// console.log("checked!");
-//1) made const again to get the list
-// const beChecked = inputName.value;
-// beChecked.classList.add("done");
-// this.classList.add("done");
-
-//2)
-// span.classList.add("done");
-// listItem.classList.add("done");
-// node.classList.add("done");
-// }
-
 function delBtn() {
-  // console.log(this.parentNode);
-  // console.log(child);
   lists.removeChild(this.parentNode); //parentNode = "ul"
 
   //ちなみにアロー関数で定義すると、定義した時点でどこから呼ばれたかで指すthisが決まる
 }
 
-function editBtn(e) {
-  console.log("editしてやるで");
-  const target = e.target.classList.contains("edit");
-  // if (target) {
-  //   let val = e.target.parentNode.firstChild.innerHTML;
-  //   // alert(val);
-  //   e.target.parentNode.innerHTML = `
-  // <input type="checkbox" class="check" />
-  //   <span class="inputed_name">
-  //   ${inputName.value}
-  //   </span>
-  //   <span class="inputed_num">
-  //   ${inputNum.value}
-  //   </span>
-  //   <span class="material-symbols-outlined delete">delete</span>
-  //   <span class="material-symbols-outlined edit">
-  //   edit_note
-  //   </span>
-  //    `;
-  // }
-}
+// const edit = e.target.classList.contains("edit");　//editのクラスが含まれている
+// e.target.onclick = function editSave() {
+//   console.log(e);
+//   e.target.classList.contains("edit").innerHTML = `<span class="material-symbols-outlined saved"> done </span>`;
+// }
+
+// const saved = document.querySelector('.saved');
+// console.log(saved);
+// if (target = true) {
+// let val = e.target.parentNode.firstChild.innerHTML;
+
+// e.target.parentNode.innerHTML = `
+// <input type="checkbox" class="check" />
+//   <span class="inputed_name">
+//    ${inputName.value}
+//    </span>
+//    <span class="inputed_num">
+//    ${inputNum.value}
+//    </span>
+//    <span class="material-symbols-outlined delete">delete</span>
+//    <span class="material-symbols-outlined edit">
+//    edit_note
+//    </span>
+//     `;
