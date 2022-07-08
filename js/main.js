a8209e037ae169b4c59f4f7e7b4a6a2607f0addd
Hikari7 <h.kobe712@gmail.com>
2022-07-08 11:58:42 -0700

    add comments

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const lists = document.querySelector(".list"); // ul
 // const editList = document.querySelector('.edit_list')
 
 //functionは外だったら反映するので、一旦引数に渡して外で内容を書いていく
-
 addBtn.addEventListener("click", addList);
 
 function addList(e) {
@@ -20,13 +19,10 @@ function addList(e) {
     <span class="inputed_num">  
     ${inputNum.value}
     </span>
-
           <input type="text" class ="text_edit"/>
           <input type="number"  class ="num_edit"/>
     <span class="material-symbols-outlined delete_btn">delete</span>
-    <span class="material-symbols-outlined edit_btn">
-    edit_note
-    </span>
+    <span class="material-symbols-outlined edit_btn">edit_note</span>
     `),
   ];
 
@@ -36,83 +32,70 @@ function addList(e) {
 
   lists.append(listItem);
 
-  // inputName.setAttribute('input', 'readonly');
-
   const deletes = document.querySelectorAll(".delete_btn").forEach((del) => {
     del.addEventListener("click", delBtn);
   });
 
-  // const edits = document.querySelectorAll(".edit_btn").forEach((edit) => {
-  //   edit.addEventListener("click", editMode);
-  // });
-
   const inputedName = document.querySelector(".inputed_name");
   const inputedNum = document.querySelector(".inputed_num");
-
   const textEdit = document.querySelectorAll(".text_edit");
   const numEdit = document.querySelectorAll(".num_edit");
 
-  document.querySelectorAll(".edit_btn").forEach((edit) => {
-    console.log(edit);
-    edit.addEventListener("click", () => {
-      console.log("edit");
-      textEdit.forEach(function (el) {
-        inputedName.textContent = "";
-        el.classList.remove("text_edit");
-        el.classList.add("inputed_name_edit");
-      });
-      numEdit.forEach(function (el2) {
-        inputedNum.textContent = "";
-        el2.classList.remove("num_edit");
-        el2.classList.add("inputed_num_edit");
-      });
-    });
+ 
+
+  // const edits = e.target.classList.contains(".edit_btn").forEach((edit) => {
+  //   edit.addEventListener("click", editBtn);
+  // });
+
+
+  const edits = document.querySelectorAll(".edit_btn").forEach((edit) => {
+    edit.addEventListener("click", editBtn);
   });
 
-  // function editMode() { 　//このfunctionの中で書いているからうまくいかない、全部ぜんぶとって来てしまう？
-  //   console.log("editmode");
-  //この時点でeditボタン押したことになってる
-  // const edits = document
-  //   .querySelectorAll(".edit_mode")
-  //   .forEach((edi) => {
-  //     console.log(edi);
-  //     edi.classList.remove("edit_mode"); //formからのDOMを取るようにする
-  //     //valueを更新する
-  //   });
+  function editBtn() {
+   
+    console.log("I'm editting!");
+    textEdit.forEach(function (el) {
+      inputedName.textContent = "";
+      el.classList.toggle("text_edit");
+      el.classList.toggle("inputed_name_edit");
+    });
+    numEdit.forEach(function (el2) {
+      inputedNum.textContent = "";
+      el2.classList.toggle("num_edit");
+      el2.classList.toggle("inputed_num_edit");
+    });
+  }
 
-  // const editParent = document.querySelector('.edit_mode')
-  // let editChildren = editParent.querySelectorAll(":scope > form");
-  // editChildren.forEach(item => item.classList.remove("edit_mode"));
-  // }
+  // const edits = document.querySelectorAll(".edit_btn");
+  // edits.forEach(function (edit) {
+  //   // console.log(edit);
+  //   edit.addEventListener("click", () => {
+  //     console.log("edit");
+  //     edit.innerHTML = `<span class="material-symbols-outlined">done</span>`;
+
+  //     textEdit.forEach(function (el) {
+  //       inputedName.textContent = "";
+  //       el.classList.toggle("text_edit");
+  //       el.classList.toggle("inputed_name_edit");
+  //     });
+  //     numEdit.forEach(function (el2) {
+  //       inputedNum.textContent = "";
+  //       el2.classList.toggle("num_edit");
+  //       el2.classList.toggle("inputed_num_edit");
+  //     });
+  //   });
+    // edit.forEach("click", (ed) => {
+    //   edit.addEventListener("click"),
+    //     () => {
+    //       edit.innerHTML = `<span class="material-symbols-outlined edit_btn">edit_note</span>`;
+    //     };
+    // });
+  // });
 }
 
 function delBtn() {
-  console.log("delするよ");
-  lists.removeChild(this.parentNode); //parentNode = "ul"
+  console.log("I'm deleting!");
+  // lists.removeChild(this.parentNode); //parentNode = "ul"
   //ちなみにアロー関数で定義すると、定義した時点でどこから呼ばれたかで指すthisが決まる
 }
-
-// const edit = e.target.classList.contains("edit");　//editのクラスが含まれている
-// e.target.onclick = function editSave() {
-//   console.log(e);
-//   e.target.classList.contains("edit").innerHTML = `<span class="material-symbols-outlined saved"> done </span>`;
-// }
-
-// const saved = document.querySelector('.saved');
-// console.log(saved);
-// if (target = true) {
-// let val = e.target.parentNode.firstChild.innerHTML;
-
-// e.target.parentNode.innerHTML = `
-// <input type="checkbox" class="check" />
-//   <span class="inputed_name">
-//    ${inputName.value}
-//    </span>
-//    <span class="inputed_num">
-//    ${inputNum.value}
-//    </span>
-//    <span class="material-symbols-outlined delete">delete</span>
-//    <span class="material-symbols-outlined edit">
-//    edit_note
-//    </span>
-//     `;
